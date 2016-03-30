d4a6425968a5abd756282bd5a2714ccf035f2a88
ioji <ioji@chervonagura.com.ar>
2016-03-30 12:18:08 -0300

    Save neighbors on each particle

--- cim/cim.rb
+++ cim/cim.rb
@@ -3,9 +3,7 @@
 require 'pp'
 require 'set'
 require './parser.rb'
-require './printer.rb'
 include Parser
-include Printer
 
 class State
   attr_reader :grid_size, :cell_size
@@ -36,10 +34,15 @@ class Particle
       @y = nil
       @vx = nil
       @vy = nil
+      @neighbors = Set.new
   end
 
   def to_s()
     "id: #{@id}, radius: #{@radius}, color: #{@color}, x: #{@x}, y: #{@y}"
+  end
+
+  def add_neighbor(particle)
+    @neighbors.add(particle)
   end
 end
 
@@ -96,57 +99,19 @@ def grid_position(pos, cell_size)
   return (pos/cell_size).floor
 end
 
-# Compares all the particles in the gred between each other and prints the close ones to a file
-def distance_between_all_particles(state, rc)
-  start = Time.now
-  particles = state.particles
-  close_particles = initialize_close_particles_hash(particles)
-
-  particles.each_with_index do |p, index|
-    for i in index+1..particles.length-1
-      if are_particles_neighbors(p, particles[i], rc)
-        close_particles[p.id].add(particles[i].id)
-        close_particles[particles[i].id].add(p.id)
-      end
-    end
-  end
-
-  finish = Time.now
-  puts "Force Brute Time: #{finish - start}"
-
-  print_to_file(close_particles)
-end
-
-# Returns a hash with all the particles ids as keys and an empty Set as value
-def initialize_close_particles_hash(particles)
-  close_particles = {}
-
-  particles.each do |p|
-    close_particles[p.id] = Set.new
-  end
-
-  return close_particles
-end
-
 # Implementation of the Cell Index Method
 def cell_index_method(state, rc, with_boundaries)
-  start = Time.now
   grid = state.grid
-  close_particles = initialize_close_particles_hash(state.particles)
   m = (state.grid_size / state.cell_size).round
 
   grid.keys.each do |cell|
-    evaluate_neighbors(grid, close_particles, cell, cell, rc)
-    evaluate_neighbors(grid, close_particles, cell, generate_neighbor_cell(cell.i, cell.j + 1, with_boundaries, m), rc)
-    evaluate_neighbors(grid, close_particles, cell, generate_neighbor_cell(cell.i + 1, cell.j - 1, with_boundaries, m), rc)
-    evaluate_neighbors(grid, close_particles, cell, generate_neighbor_cell(cell.i + 1, cell.j, with_boundaries, m), rc)
-    evaluate_neighbors(grid, close_particles, cell, generate_neighbor_cell(cell.i + 1, cell.j + 1, with_boundaries, m), rc)
+    evaluate_neighbors(grid, cell, cell, rc)
+    evaluate_neighbors(grid, cell, generate_neighbor_cell(cell.i, cell.j + 1, with_boundaries, m), rc)
+    evaluate_neighbors(grid, cell, generate_neighbor_cell(cell.i + 1, cell.j - 1, with_boundaries, m), rc)
+    evaluate_neighbors(grid, cell, generate_neighbor_cell(cell.i + 1, cell.j, with_boundaries, m), rc)
+    evaluate_neighbors(grid, cell, generate_neighbor_cell(cell.i + 1, cell.j + 1, with_boundaries, m), rc)
   end
 
-  finish = Time.now
-  puts "Cell Index Method Time: #{finish - start}"
-
-  print_to_file(close_particles)
 end
 
 # Generates a cell based on i, j. Makes a cell around the grid if with_boundaries is true
@@ -160,15 +125,15 @@ def generate_neighbor_cell(i, j, with_boundaries, m)
 end
 
 # Adds to close_particles all the particles of cell close to the ones of other_cell
-def evaluate_neighbors(grid, close_particles, cell, other_cell, rc)
+def evaluate_neighbors(grid, cell, other_cell, rc)
   if grid.has_key?(other_cell)
     particles = grid[cell]
     other_particles = grid[other_cell]
 
     particles.each do |p1|
       other_particles.each do |p2|
-        if p1.id != p2.id && are_particles_neighbors(p1, p2, rc)
-          close_particles[p1.id].add(p2.id)
+        if are_particles_neighbors(p1, p2, rc)
+          p1.add_neighbor(p2)
         end
       end
     end
@@ -185,7 +150,7 @@ m = ARGV[0].to_i
 rc = ARGV[1].to_f
 raise ArgumentError, "The amount of cells and particle interaction radius are both required" if m == 0 || rc == 0
 
-state = parse_input("samples/Static100.txt", "samples/Dynamic100.txt", m)
+state = parse_input("../randstatic.txt", "../randdynamic.txt", m)
 rmax = state.particles.max {|a, b| a.radius <=> b.radius}.radius
 raise ArgumentError, "Wrong argument value: L/M > rc + 2*rmax" if state.cell_size <= rc + 2 * rmax
 
@@ -193,4 +158,3 @@ with_boundaries = ARGV[2].to_s == "b" ? true : false
 
 align_grid(state)
 cell_index_method(state, rc, with_boundaries)
-distance_between_all_particles(state, rc)
